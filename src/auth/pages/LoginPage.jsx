bfbc1ad4b28565f04d25e1c657b900c6139dc9bc
ACastilloSantiago <santiagorodrigec@gmail.com>
2024-05-31 13:32:35 -0300

    feat: add register page

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -2,69 +2,52 @@ import { Link as RouterLink } from 'react-router-dom';
 
 import { Google } from '@mui/icons-material';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { AuthLayout } from '../layout/AuthLayout';
 
 export const LoginPage = () => {
   return (
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
-    >
-      <Grid
-        item
-        className="box-shadow"
-        xs={3}
-        sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}
-      >
-        <Typography variant="h5" sx={{ mb: 1 }}>
-          Login
-        </Typography>
+    <AuthLayout title="Iniciar Sesión">
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ marginTop: 2 }}>
+            <TextField
+              label="Correo"
+              type="email"
+              placeholder="correo@gmail.com"
+              fullWidth
+            />
+          </Grid>
 
-        <form>
-          <Grid container>
-            <Grid item xs={12} sx={{ marginTop: 2 }}>
-              <TextField
-                label="Correo"
-                type="email"
-                placeholder="correo@gmail.com"
-                fullWidth
-              />
-            </Grid>
+          <Grid item xs={12} sx={{ marginTop: 2 }}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              placeholder="Contraseña"
+              fullWidth
+            />
+          </Grid>
 
-            <Grid item xs={12} sx={{ marginTop: 2 }}>
-              <TextField
-                label="Contraseña"
-                type="password"
-                placeholder="Contraseña"
-                fullWidth
-              />
-
-              <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
-                <Grid item xs={12} sm={6}>
-                  <Button variant="contained" fullWidth>
-                    Login
-                  </Button>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <Button variant="contained" fullWidth>
-                    <Google />
-                    <Typography sx={{ ml: 1 }}>Google</Typography>
-                  </Button>
-                </Grid>
+            <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+              <Grid item xs={12} sm={6}>
+                <Button variant="contained" fullWidth>
+                  Login
+                </Button>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Button variant="contained" fullWidth>
+                  <Google />
+                  <Typography sx={{ ml: 1 }}>Google</Typography>
+                </Button>
               </Grid>
             </Grid>
 
-            <Grid container direction="row" justifyContent="end">
-              <Link component={RouterLink} to="/auth/register">
-                Crear una cuenta
-              </Link>
-            </Grid>
+          <Grid container direction="row" justifyContent="end">
+            <Link component={RouterLink} to="/auth/register">
+              Crear cuenta
+            </Link>
           </Grid>
-        </form>
-      </Grid>
-    </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
   );
 };
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,15 @@
+import { Typography } from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
 
 export const JournalPage = () => {
   return (
-    <h1>JournalPage</h1>
-  )
-}
+    <JournalLayout>
+      <Typography>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+        perferendis fugit doloremque consectetur mollitia corporis, nobis quae
+        saepe asperiores adipisci illo laborum vitae obcaecati eos et
+        voluptates. Odio, similique sequi?
+      </Typography>
+    </JournalLayout>
+  );
+};
