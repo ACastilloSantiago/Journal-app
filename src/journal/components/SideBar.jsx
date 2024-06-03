eb5159ec5a8dda65e244ff5dd9eb9946debcde3f
ACastilloSantiago <santiagorodrigec@gmail.com>
2024-06-03 20:25:35 -0300

    feat: add sidebar

---
+++ src/journal/components/SideBar.jsx
@@ -0,0 +1,71 @@
+import { TurnedInNot } from '@mui/icons-material';
+import {
+  Box,
+  Divider,
+  Drawer,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+
+export const SideBar = ({ drawerWidth }) => {
+  return (
+    <Box
+      component="nav"
+      sx={{
+        width: {
+          sm: drawerWidth,
+        },
+        flexShrink: {
+          sm: 0,
+        },
+      }}
+    >
+      <Drawer
+        variant="permanent" //temporary
+        open
+        sx={{
+          display: {
+            xs: 'block',
+          },
+          '& .MuiDrawer-paper': {
+            boxSizing: 'border-box',
+            width: drawerWidth,
+          },
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Santiago Castillo
+          </Typography>
+        </Toolbar>
+
+        <Divider />
+        <List>
+          {['Enero', 'Febrero', 'Marzo', 'Abril'].map((text) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <TurnedInNot />
+                </ListItemIcon>
+                <Grid container>
+                  <ListItemText primary={text} />
+                  <ListItemText
+                    secondary={
+                      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit quam reiciendis, qui quae voluptate distinctio impedit labore voluptatibus esse eos dolorem nobis '
+                    }
+                  />
+                </Grid>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
+};
