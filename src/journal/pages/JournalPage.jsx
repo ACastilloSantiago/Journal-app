e2f5427c5b113c9034aed979ddc42edefe246c5b
ACastilloSantiago <santiagorodrigec@gmail.com>
2024-06-04 15:47:09 -0300

    feat: add button

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,15 +1,42 @@
-import { Typography } from '@mui/material';
+import { IconButton, Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NoteView, NothingSelectedView } from '../views';
+import { AddOutlined } from '@mui/icons-material';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
+      {/* <Typography>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
         perferendis fugit doloremque consectetur mollitia corporis, nobis quae
         saepe asperiores adipisci illo laborum vitae obcaecati eos et
         voluptates. Odio, similique sequi?
-      </Typography>
+      </Typography> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': {
+            opacity: 0.9,
+            backgroundColor: 'error.main',
+          },
+          position: 'fixed',
+          bottom: 50,
+          right: 50,
+        }}
+      >
+        <AddOutlined
+          sx={{
+            fontSize: 30,
+          }}
+        />
+      </IconButton>
+
+      {/* <NothingSelectedView /> */}
+
+      <NoteView />
     </JournalLayout>
   );
 };
