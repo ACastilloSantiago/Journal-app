0bbf5116465a5d526022e1ae4408926edc804195
ACastilloSantiago <santiagorodrigec@gmail.com>
2024-06-07 15:57:31 -0300

    feat: add provider

--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,15 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 import { JournalApp } from './JournalApp';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
