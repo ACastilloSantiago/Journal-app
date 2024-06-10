5d6a9c77436b8a2ea3c0c89b3cda51ff2899b66a
ACastilloSantiago <santiagorodrigec@gmail.com>
2024-06-10 20:11:46 -0300

    feat: add state handling

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -4,16 +4,30 @@ export const authSlice = createSlice({
   name: 'auth',
   initialState: {
     status: 'not-authenticated', // 'checking', 'not-authenticated', 'authenticated'
-    uid: 'null',
+    uid: null,
     email: null,
     displayName: null,
     photoURL: null,
     errorMessage: null,
   },
   reducers: {
-    login: (state, action) => {},
+    login: (state, { payload }) => {
+      state.status = 'not-authenticated';
+      state.uid = payload.uid;
+      state.email = payload.email;
+      state.displayName = payload.displayName;
+      state.photoURL = payload.photoURL;
+      state.errorMessage = null;
+    },
 
-    logout: (state, payload) => {},
+    logout: (state, { payload }) => {
+      state.status = 'not-authenticated';
+      state.uid = null;
+      state.email = null;
+      state.displayName = null;
+      state.photoURL = null;
+      state.errorMessage = payload.errorMessage;
+    },
 
     checkingCredentials: (state) => {
       state.status = 'checking';
