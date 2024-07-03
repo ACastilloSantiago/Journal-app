fc3301468f1b4107066a5655e71de0ac9fbad40f
ACastilloSantiago <santiagorodrigec@gmail.com>
2024-07-03 16:43:36 -0300

    feat: :sparkles: add thunks for login and register with email & password

--- src/store/thunks.js
+++ src/store/thunks.js
@@ -1,4 +1,8 @@
-import { signInWithGoogle } from '../firebase/providers';
+import {
+  loginWithEmailPassword,
+  registerUserWithEmailPassword,
+  signInWithGoogle,
+} from '../firebase/providers';
 import { checkingCredentials, login, logout } from './auth/authSlice';
 
 export const checkingAuthentication = (email, password) => {
@@ -16,5 +20,53 @@ export const startGoogleSignIn = () => {
     if (!result.ok) return dispatch(logout(result.errorMessage));
     // * clean
     dispatch(login(result));
+  };
+};
+
+export const startCreatingUserWithEmailPassword = ({
+  email,
+  password,
+  displayName,
+}) => {
+  return async (dispatch) => {
+    dispatch(checkingCredentials());
+
+    const { ok, uid, photoURL, errorMessage } =
+      await registerUserWithEmailPassword({
+        email,
+        password,
+        displayName,
+      });
+
+    if (!ok) return dispatch(logout({ errorMessage }));
+
+    return dispatch(
+      login({
+        uid,
+        displayName,
+        email,
+        photoURL,
+      })
+    );
+  };
+};
+
+export const startLoginWithcEmailPassword = ({ email, password }) => {
+  return async (dispatch) => {
+       dispatch(checkingCredentials());
+
+
+    const { ok, uid, photoURL, displayName, errorMessage } =
+      await loginWithEmailPassword({ email, password });
+    if (!ok) return dispatch(logout({errorMessage}));
+
+    return dispatch(
+      login({
+        uid,
+        displayName,
+        email,
+        photoURL,
+      })
+    );
   };
 };
