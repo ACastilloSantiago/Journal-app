1753350d16b886e38dc057a08a7538e5570d8ea0
ACastilloSantiago <santiagorodrigec@gmail.com>
2024-07-04 15:46:27 -0300

    feat: :sparkles: add checking loader layout &  ternary authenticated

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,16 +1,46 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
 import { JournalRoutes } from '../journal/routes/JournalRoutes';
+import { useSelector, useDispatch } from 'react-redux';
+import { CheckingAuth } from '../ui';
+import { useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { FirebaseAuth } from '../firebase/config';
+import { login, logout } from '../store/auth';
 
 export const AppRouter = () => {
+  const { status } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(FirebaseAuth, async (user) => {
+      if (!user) return dispatch(logout());
+      console.log(user);
+      const { email, uid, photoURL, displayName } = user;
+      dispatch(login({ email, uid, photoURL, displayName }));
+    });
+  }, []);
+
+  if (status === 'checking') {
+    return <CheckingAuth />;
+  }
+
   return (
     <Routes>
+      {status === 'authenticated' ? (
+        <Route path="/*" element={<JournalRoutes />} />
+      ) : (
+        <Route path="auth/*" element={<AuthRoutes />} />
+      )}
+
+      <Route path="/*" element={<Navigate to="auth/login" />} />
+
       {/*Login y Registro  */}
-      <Route path='auth/*' element={<AuthRoutes />} />
+      {/* <Route path="auth/*" element={<AuthRoutes />} /> */}
 
       {/* Journal App */}
-      <Route path='/*' element={<JournalRoutes />} />
-      
+      {/* <Route path="/*" element={<JournalRoutes />} /> */}
     </Routes>
   );
 };
