6e29b2ffa2854f0b02faa4a9b154f4c6c4d2c162
ACastilloSantiago <santiagorodrigec@gmail.com>
2024-07-04 16:14:51 -0300

    feat: :sparkles: embed usecheckauth in app router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,26 +1,13 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
+
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
 import { JournalRoutes } from '../journal/routes/JournalRoutes';
-import { useSelector, useDispatch } from 'react-redux';
 import { CheckingAuth } from '../ui';
-import { useEffect } from 'react';
-import { onAuthStateChanged } from 'firebase/auth';
-import { FirebaseAuth } from '../firebase/config';
-import { login, logout } from '../store/auth';
+import { useCheckAuth } from '../hooks';
 
 export const AppRouter = () => {
-  const { status } = useSelector((state) => state.auth);
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    onAuthStateChanged(FirebaseAuth, async (user) => {
-      if (!user) return dispatch(logout());
-      console.log(user);
-      const { email, uid, photoURL, displayName } = user;
-      dispatch(login({ email, uid, photoURL, displayName }));
-    });
-  }, []);
+  
+  const { status } = useCheckAuth;
 
   if (status === 'checking') {
     return <CheckingAuth />;
