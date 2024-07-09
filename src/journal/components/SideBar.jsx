bdccddbffe563c67a6dd3f8b471313f18eb17e35
ACastilloSantiago <santiagorodrigec@gmail.com>
2024-07-09 13:30:40 -0300

    feat: :sparkles: sidebar item incorporated and use state notes

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -14,9 +14,12 @@ import {
   Toolbar,
   Typography,
 } from '@mui/material';
+import { SideBarItem } from './SideBarItem';
 
 export const SideBar = ({ drawerWidth }) => {
   const { displayName } = useSelector((state) => state.auth);
+  const { notes } = useSelector((state) => state.journal);
+
   return (
     <Box
       component="nav"
@@ -50,22 +53,8 @@ export const SideBar = ({ drawerWidth }) => {
 
         <Divider />
         <List>
-          {['Enero', 'Febrero', 'Marzo', 'Abril'].map((text) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <TurnedInNot />
-                </ListItemIcon>
-                <Grid container>
-                  <ListItemText primary={text} />
-                  <ListItemText
-                    secondary={
-                      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit quam reiciendis, qui quae voluptate distinctio impedit labore voluptatibus esse eos dolorem nobis '
-                    }
-                  />
-                </Grid>
-              </ListItemButton>
-            </ListItem>
+          {notes.map((note) => (
+            <SideBarItem key={note.id} {...note} />
           ))}
         </List>
       </Drawer>
