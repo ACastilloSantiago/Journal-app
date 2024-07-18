a257ec07b71ba12976f35b77e237cd5b744206fd
ACastilloSantiago <santiagorodrigec@gmail.com>
2024-07-18 16:43:56 -0300

    feat: :sparkles: add setup of getEnverionments

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,8 @@
 import 'whatwg-fetch';
+
+require('dotenv').config({
+  path: '.env.test',
+});
+jest.mock('./src/helpers/getEnvironments', () => ({
+  getEnvironments: () => ({ ...process.env }),
+}));
